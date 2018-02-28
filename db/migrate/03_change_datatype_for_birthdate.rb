acb3fdf901a9fc69cc76b551038494f55273ab80
Berry  Semexan <bsemexan@gmail.com>
2018-02-28 20:03:49 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,5 @@
 class CHangeDataTypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
-    create_table :students do |t|
-      t.integer :grade
-      t.string :birthdate
-    end
+    add_column :students,:birthdate,:datetime
   end
 end
